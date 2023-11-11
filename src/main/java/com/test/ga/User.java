1b54915363a08f89f4ab524f8221b7ced6918452
dlopez <dalro1028@hotmail.com>
2023-11-11 15:35:44 -0500

    First Commit

---
+++ src/main/java/com/test/ga/User.java
@@ -0,0 +1,13 @@
+package com.test.ga;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class User {
+
+    @GetMapping("/user")
+    public String saludo(){
+        return "Hola Mundo!";
+    }
+}
