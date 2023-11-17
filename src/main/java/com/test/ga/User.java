75eaf68c09c79052a1b88c741ca95a70c5e570ab
10Daniel10 <85538502+10Daniel10@users.noreply.github.com>
2023-11-16 22:54:31 -0500

    Update User.java

--- src/main/java/com/test/ga/User.java
+++ src/main/java/com/test/ga/User.java
@@ -3,11 +3,15 @@ package com.test.ga;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Properties;
+import java.io.InputStream;
 
 @RestController
 public class User {
 
 Properties prop = new Properties();
+InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties");
+prop.load(input);
+
 String name = prop.getProperty("nombre");
 String surename = prop.getProperty("apellido");
 
